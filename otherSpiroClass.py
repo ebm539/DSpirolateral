5567ea0924a77dd3aae047537878a302da735132
Lachlan Paulsen <Lachlan_Paulsen@paraparaumucollege.school.nz>
2019-04-30 14:35:44 +1200

    seperated spirolateral storage and spirolateral drawing into 2 classes

--- otherSpiroClass.py
+++ otherSpiroClass.py
@@ -1,28 +1,76 @@
 import turtle
 import time
 
-class Spirolateral():
-    def __init__(self, name, timeTable, angle, screen, centerPos, scale):
+class SpirolateralDrawer():
+    def __init__(self, screen, scale):
         self.screen = screen
-        self.name = str(name)
-        self.timeTable = int(timeTable)
-        self.angle = int(angle)
         self.turtleObject = turtle.RawTurtle(self.screen)
         self.turtleObject.hideturtle()
         self.ghostTurtle = turtle.RawTurtle(self.screen)
         self.ghostTurtle.hideturtle()
         self.ghostTurtle.speed(0)
         self.turtleObject.speed(0)
-        self.centerPos = centerPos
+        self.centerPos = (0,0)
         self.scale = scale
-        self.dRootList = self.genDrootList()
 
         self.centerTurtle = turtle.RawTurtle(self.screen)
         self.centerTurtle.shape("circle")
         self.centerTurtle.penup()
         self.centerTurtle.goto(self.centerPos)
 
-    def genDrootList(self):
+    def drawBasicSpiro(self, spiroObject, turtleObject, startPos, visible):
+        xLocationList = []
+        yLocationList = []
+
+        turtleObject.penup()
+        turtleObject.goto(startPos)
+        if visible:
+            turtleObject.pendown()
+
+        while True:
+            for distance in spiroObject.dRootList:
+                turtleObject.forward(distance * self.scale)
+                turtleObject.right(spiroObject.angle)
+
+            currentPosx, currentPosy = turtleObject.pos()
+            currentPosx = round(currentPosx, 3)
+            currentPosy = round(currentPosy, 3)
+
+            xLocationList.append(currentPosx)
+            yLocationList.append(currentPosy)
+
+            xLocationList.append(currentPosx)
+            if currentPosx == startPos[0] and currentPosy == startPos[1]:
+                break
+
+        return xLocationList, yLocationList
+
+    def loadSpiro(self, spiroObject):
+        self.clearScreen()
+        self.screen.tracer(False)
+        xLocationList, yLocationList = self.drawBasicSpiro(spiroObject, self.ghostTurtle, self.centerPos, False)
+        print("Length of location list:", len(xLocationList))
+        minXvalue, maxXvalue = (min(xLocationList), max(xLocationList))
+        minYvalue, maxYvalue = (min(yLocationList), max(yLocationList))
+
+        newStartX = round(self.centerPos[0] - (((minXvalue - self.centerPos[0]) + (maxXvalue - self.centerPos[0])) / 2), 3)
+        newStartY = round(self.centerPos[1] - (((minYvalue - self.centerPos[1]) + (maxYvalue - self.centerPos[1])) / 2), 3)
+        self.drawBasicSpiro(spiroObject, self.turtleObject, (newStartX, newStartY), True)
+        self.screen.tracer(True)
+
+    def clearScreen(self):
+        print("Called clear screen")
+        self.turtleObject.clear()
+        self.ghostTurtle.clear()
+        pass
+
+class Spirolateral():
+    def __init__(self, timeTable, angle):
+        self.timeTable = timeTable
+        self.angle = angle
+        self.dRootList = self.genDrootList(self.timeTable)
+
+    def genDrootList(self, timeTable):
         rootList = []
         x = 0
         while True:
@@ -35,55 +83,6 @@ class Spirolateral():
                 rootList.append(dRoot)
 
         return(rootList)
-
-    def drawBasicSpiro(self, turtleObject, startPos, visible):
-        print((self.dRootList))
-        self.genDrootList()
-        xLocationList = []
-        yLocationList = []
-
-        turtleObject.penup()
-        turtleObject.goto(startPos)
-        if visible:
-            turtleObject.pendown()
-
-        while True:
-            for distance in self.dRootList:
-                turtleObject.forward(distance * self.scale)
-                turtleObject.right(self.angle)
-
-            currentPosx, currentPosy = turtleObject.pos()
-            currentPosx = round(currentPosx, 3)
-            currentPosy = round(currentPosy, 3)
-            print(currentPosx, currentPosy)
-
-            xLocationList.append(currentPosx)
-            yLocationList.append(currentPosy)
-
-            xLocationList.append(currentPosx)
-            if currentPosx == startPos[0] and currentPosy == startPos[1]:
-                print("NANI?")
-                break
-
-        return xLocationList, yLocationList
-
-    def drawCenteredSpiro(self):
-        self.screen.tracer(False)
-        xLocationList, yLocationList = self.drawBasicSpiro(self.ghostTurtle, self.centerPos, False)
-        minXvalue, maxXvalue = (min(xLocationList), max(xLocationList))
-        minYvalue, maxYvalue = (min(yLocationList), max(yLocationList))
-
-        newStartX = round(self.centerPos[0] - (((minXvalue - self.centerPos[0]) + (maxXvalue - self.centerPos[0])) / 2), 3)
-        newStartY = round(self.centerPos[1] - (((minYvalue - self.centerPos[1]) + (maxYvalue - self.centerPos[1])) / 2), 3)
-        self.drawBasicSpiro(self.turtleObject, (newStartX, newStartY), True)
-        self.screen.tracer(True)
-
-    def clearScreen(self):
-        self.screen.clear()
-        self.turtleObject.penup()
-        self.turtleObject.goto(0,0)
-        self.turtleObject.pendown()
-        self.ghostTurtle.goto(0,0)
 
 
 
