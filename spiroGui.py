47251f0581760f5ce1f275db780ca93ab630cb57
Lachlan Paulsen <lachlan_paulsen@paraparaumucollege.school.nz>
2019-05-03 21:52:07 +1200

    Code is not quite functional, but almost. Shifting everything into a class

--- spiroGui.py
+++ spiroGui.py
@@ -14,65 +14,57 @@ except ModuleNotFoundError:
 import time
 import turtle
 
+class SpiroGui():
+    def __init__():
+        self.root = tk.Tk()
+        self.spiroList = [1,2,3,4,5]
 
-root = tk.Tk()
+        self.optionFrame = tk.Frame(self.root)
+        self.optionFrame.grid(row=0)
 
-spiroList = []
+        self.turtleScreenCanvas = tk.Canvas(self.root, height=400, width)
+        self.turtleScreenCanvas.grid(row=1)
+        self.turtleScreen = turtle.TurtleScreen(self.turtleScreenCanvas)
+        self.turtleScreen.screensize(400, 400)
 
-def buildGui():
-    global canvas
-    global prevCancelButton, nextConfirmButton, addButton
-    optionFrame = tk.Frame(root)
-    optionFrame.grid(row=0)
+        self.controlFrame = tk.Frame(self.root)
+        self.controlFrame.grid(row=2)
 
-    canvas = tk.Canvas(root, height=400, width=400)
-    canvas.grid(row=1)
+        self.addButton = tk/Button(self.optionFrame, text="Add new...", command=self.addNewDialog)
+        self.addButton.grid(row=0, column=0)
 
-    controlFrame = tk.Frame(root)
-    controlFrame.grid(row=2)
+        self.deleteButton = tk.Button(self.optionFrame, text="Delete")
+        self.deleteButton.grid(row=0, column=1)
 
-    addButton = tk.Button(optionFrame, text="Add new...", command=addNewDialog)
-    addButton.grid(row=0, column=0)
+        self.prevCancelButton = tk.Button(self.controlFrame, text="<- Prev", command=self.previousSpiro)
+        self.prevCancel.grid(row=2, column=0)
 
-    deleteButton = tk.Button(optionFrame, text="Delete")
-    deleteButton.grid(row=0, column=1)
+        self.nextConfirmButton = tk.Button(self.controlFrame, text="Next ->", command=self.nextSpiro)
+        self.nextConfirmButton.grid(row=2, column=2)
 
-    prevCancelButton = tk.Button(controlFrame, text="<- Prev", command=previousSpiro)
-    prevCancelButton.grid(row=2, column=0)
+        self.spiroDrawer = spiroModule.SpirolateralDrawer(self.turtleScreen, 10)
 
-    nextConfirmButton = tk.Button(controlFrame, text="Next ->", command=nextSpiro)
-    nextConfirmButton.grid(row=2, column=2)
+    def previousSpiro(self):
+        print("Prev")
+    def nextSpiro(self):
+        print("Next")
 
-def previousSpiro():
-    print("Prev")
-def nextSpiro():
-    print("Next")
-
-def addNewDialog():
-    prevCancelButton.configure(text="Cancel", command=addCancel)
-    nextConfirmButton.configure(text="Confirm", command=addNewSpirolateral)
-    addButton.configure(relief=tk.SUNKEN)
-
-
-def addNewSpirolateral():
-    print("Spiro added ;)")
-    prevCancelButton.configure(text="<- Prev", command=previousSpiro)
-    nextConfirmButton.configure(text="Next ->", command=nextSpiro)
-
-def addCancel():
-    print("Canceling spiro addition")
-    prevCancelButton.configure(text="<- Prev", command=previousSpiro)
-    nextConfirmButton.configure(text="Next ->", command=nextSpiro)
-    addButton.configure(relief=tk.RAISED)
+    def addNewDialog(self):
+        self.prevCancelButton.configure(text="Cancel", command=self.addCancel)
+        self.nextConfirmButton.configure(text="Confirm", command=self.addNewSpirolateral)
+        self.addButton.configure(relief=tk.SUNKEN)
 
 
+    def addNewSpirolateral(self):
+        print("Spiro added ;)")
+        self.prevCancelButton.configure(text="<- Prev", command=self.previousSpiro)
+        self.nextConfirmButton.configure(text="Next ->", command=self.nextSpiro)
 
-#def addNewSpiro()
-
-buildGui()
-
-screen = turtle.TurtleScreen(canvas)
-screen.screensize(400, 400)
+    def addCancel(self):
+        print("Canceling spiro addition")
+        self.prevCancelButton.configure(text="<- Prev", command=self.previousSpiro)
+        self.nextConfirmButton.configure(text="Next ->", command=self.nextSpiro)
+        self.addButton.configure(relief=tk.RAISED)
 
 scd = spiroModule.SpirolateralDrawer(screen, 10)
 sc = spiroModule.Spirolateral(7, 45)
