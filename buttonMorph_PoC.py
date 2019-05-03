532959f9e823c7076c83b9538975d7f4baf52417
Lachlan Paulsen <lachlan_paulsen@paraparaumucollege.school.nz>
2019-05-03 12:22:06 +1200

    Added files and better progress. Made main and a button morph PoC

--- buttonMorph_PoC.py
+++ buttonMorph_PoC.py
@@ -1,11 +1,27 @@
 ##Decided to seperate the spirolateral object from the parts that controlled the drawing of the spirolateral
 
 import otherSpiroClass as spiroModule
-import tkinter as tk
+import Tkinter as tk
 import time
 import turtle
 
+
 root = tk.Tk()
+
+spiroList = []
+
+def buildGui():
+    optionFrame = tk.Frame()
+    optionFrame.grid(row=1)
+    addButton = tk.Button(optionFrame, text="Add new...")
+    addButton.grid(row=0, column=0)
+    deleteButton = tk.Button(optionFrame, text="Delete")
+    deleteButton.grid(row=0, column=1)
+
+#def addNewSpiro()
+
+buildGui()
+
 canvas = tk.Canvas()
 canvas.grid()
 btn1 = tk.Button(root, text="Help")
@@ -20,4 +36,6 @@ scd.loadSpiro(sc)
 time.sleep(2)
 sc2 = spiroModule.Spirolateral(13, 26)
 scd.loadSpiro(sc2)
+
+
 root.mainloop()
